a36829b63ba30ddf98fc5f826d52d7eb7b3ccc6f
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2021-02-03 23:18:54 +0100

    increased font size

--- shell.sh
+++ shell.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -
 
 cd /opt/bitshardshell/bitshard-riddle/bitshardshell
-fbterm --font-names='JetBrainsMono Nerd Font Mono' -s 20 -- ./target/debug/bitshardshell
+fbterm --font-names='JetBrainsMono Nerd Font Mono' -s 25 -- ./target/debug/bitshardshell
